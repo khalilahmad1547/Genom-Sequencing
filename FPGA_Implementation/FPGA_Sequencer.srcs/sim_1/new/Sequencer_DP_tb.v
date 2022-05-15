bf264294f387f0db3b5c8056df9a3d396871e9ec
Abdur Rahman <47577116+khalilahmad1547@users.noreply.github.com>
2022-05-15 21:08:40 +0500

    basic Sequencer & test bench is created.
    There are still gliches which we have to resolve.

--- FPGA_Implementation/FPGA_Sequencer.srcs/sim_1/new/Sequencer_DP_tb.v
+++ FPGA_Implementation/FPGA_Sequencer.srcs/sim_1/new/Sequencer_DP_tb.v
@@ -20,7 +20,112 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Sequencer_DP_tb(
+module Sequencer_DP_tb();
 
-    );
+// params
+parameter clk_flip = 5;
+parameter period = 10;
+
+parameter m = 16;
+parameter n = 3;
+parameter p = 4;
+parameter a = 4;
+parameter b = 4;
+
+// inputs
+reg  [m-1:0]     read;
+reg  [m-1:0]     ref_gen;
+reg  [n-1:0]     limit;
+reg              next_read;
+reg              next_ref_gen;
+reg  [a-1:0]     t_reads;
+reg  [b-1:0]     t_ref_gen;
+reg              clk;
+reg              read_cnt_reset;
+reg              ref_gen_cnt_reset;
+
+// outputs
+wire [m+p-1:0]   read_loc;
+wire             done;
+wire             reads_done;
+wire             ref_gen_done;
+wire             toStore;
+
+// input vector
+reg [m-1:0] reads_v [0:a-1];
+reg [m-1:0] ref_gens_v [0:b-1];
+
+// loop variables
+integer i = 0;
+integer j = 0;
+integer k = 0;
+integer out_file;
+
+// instant to test
+Sequencer_DP         #(.m(m),
+                       .n(n),
+                       .p(p),
+                       .a(a), 
+                       .b(b))
+Sequencer_DP_inst   (
+                       .read(read),
+                       .ref_gen(ref_gen),
+                       .limit(limit),
+                       .next_read(next_read),
+                       .next_ref_gen(next_ref_gen),
+                       .t_reads(t_reads),
+                       .t_ref_gen(t_ref_gen),
+                       .clk(clk),
+                       .read_cnt_reset(read_cnt_reset),
+                       .ref_gen_cnt_reset(ref_gen_cnt_reset),
+                       .read_loc(read_loc),
+                       .done(done),
+                       .reads_done(reads_done),
+                       .ref_gen_done(ref_gen_done),
+                       .toStore(toStore)
+                    );
+
+// clock
+initial begin
+    clk = 0;
+    limit = 3;
+    next_read = 1'b0;
+    t_reads = 10;
+    t_ref_gen = 10;
+    read_cnt_reset = 1'b0;
+    ref_gen_cnt_reset = 1'b0;
+    next_ref_gen = 1'b0;
+end
+always #clk_flip
+    clk = ~clk;
+
+initial begin
+    // feeding testing data
+    $readmemb("C:/Users/khali/Downloads/Documents/Computer_Engineering/Semester_08/FYP_III/Class/Genom-Sequencing/Data/temp_reads.txt", reads_v);
+    $readmemb("C:/Users/khali/Downloads/Documents/Computer_Engineering/Semester_08/FYP_III/Class/Genom-Sequencing/Data/temp_ref_gen.txt", ref_gens_v);
+
+    for (j=0; j<t_ref_gen; j=j+1) begin
+        ref_gen = ref_gens_v[j];
+        for (i=0; i<t_reads; i=i+1) begin
+            read = reads_v[i];
+            #period;
+        end
+    end
+end
+
+initial begin
+// writing results data
+    out_file = $fopen("C:/Users/khali/Downloads/Documents/Computer_Engineering/Semester_08/FYP_III/Class/Genom-Sequencing/Data/output.txt", "w");
+    
+    for (k=0; k<t_reads*t_ref_gen; k=k+1)
+        begin
+            $fdisplay(out_file, "%b %b", read_loc, toStore);
+            //$display("->%b %b", read_loc, toStore);
+            #period;
+        end
+    
+    $fclose(out_file);
+    $finish;
+end
+
 endmodule
--- FPGA_Implementation/FPGA_Sequencer.srcs/sources_1/new/Sequencer_DP.v
+++ FPGA_Implementation/FPGA_Sequencer.srcs/sources_1/new/Sequencer_DP.v
@@ -18,9 +18,103 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+/*
+Sequencer module is splited into two parts
+    1. Sequencer_DP (Data Path)
+    2. Sequencer_CU (Controler Unit)
+PARAMS:
+    m            -> number of bits in Read and refernce genome
+    n            -> number of bits for allowed error limit
+    p            -> number of bits required for locations of a read in the whole genome
+    a            -> number of bits required to represent total number of reads
+    b            -> number of bits required to represent total pieces of reference genome
+INPUTS:
+    read         -> m bits for read to be mapped
+    ref_gen      -> m bits for reference genome to which read is to be mapped
+    limit        -> n bits number for the allow limit of error
+    next_read    -> 1 bit signal, loads next read for processing (1), processign already loaded read.
+    next_ref_gen -> 1 bit signal, loade next piece of refernce genome (1), processing already loaded refernce genome (0). 
+    t_reads      -> a bit number, which shows total reads which have to be processed.
+    t_ref_gen    -> b bit number, which shows total pieces of refrence genome.
+OUTPUTS:
+    read_loc     -> m + p bits with first m bits of read and p bits for locations
+    done         -> 1 bit signal for weather in process(0) or done(1) for single read
+    reads_done   -> 1 bit signal for weather all reads are processed(1) or not(0)
+    ref_gen_done -> 1 bit signal for weather all pieces of reference genome is done(1), or not(0)
+    toStore      -> 1 bit number, weather it is worth to store the location data to FIFO or not
+*/
 
-
-module Sequencer_DP(
-
-    );
+module Sequencer_DP #(parameter m = 24,
+                      parameter n = 3,
+                      parameter p = 10,
+                      parameter a = 10, 
+                      parameter b = 10)
+                     (
+                      input  [m-1:0]     read,
+                      input  [m-1:0]     ref_gen,
+                      input  [n-1:0]     limit,
+                      input              next_read,
+                      input              next_ref_gen,
+                      input  [a-1:0]     t_reads,
+                      input  [b-1:0]     t_ref_gen,
+                      input              clk,
+                      input              read_cnt_reset,
+                      input              ref_gen_cnt_reset,
+                      output [m+p-1:0]   read_loc,
+                      output             done,
+                      output             reads_done,
+                      output             ref_gen_done,
+                      output             toStore
+                     );
+    // internal wires
+    wire  [p-1:0]   loc;
+    
+    // internal regs
+    
+    // instants of modules used
+    mapper    #(.m(m),
+                .n(n),
+                .p(p)) 
+    mapper_inst (.read(read),
+                 .ref_gen(ref_gen),
+                 .limit(limit),
+                 .loc(loc),
+                 .clk(clk),
+                 .read_loc(read_loc),
+                 .toStore(toStore));
+    
+    // a clock counter which will count upto 5 clock cycles             
+    clk_counter      #(.b(3))
+    clk_counter_inst  (.cycles(5),
+                       .clk(clk),
+                       .times_up(done));
+    
+    // a counter to count number of read which have processed
+    // and raise reads_done when limit reached.
+    counter      #(.b(a))
+    read_counter  (.initial_count(0),
+                   .inc(1),
+                   .max_to_count(t_reads),
+                   .enb(next_read),
+                   .upCount(next_read),
+                   .reset(read_cnt_reset),
+                   .clk(clk),
+                   .p_count(),
+                   .limit_over(reads_done));
+    
+    // a reference piece counter, and raise ref_gen_done when complete reference genome is done.
+    counter      #(.b(b))
+    ref_gen_cnter  (.initial_count(0),
+                   .inc(1),
+                   .max_to_count(t_ref_gen),
+                   .enb(next_ref_gen),
+                   .upCount(next_ref_gen),
+                   .reset(ref_gen_cnt_reset),
+                   .clk(clk),
+                   .p_count(loc),
+                   .limit_over(ref_gen_done));
+    
+    always @(posedge clk) begin
+        
+    end
 endmodule
